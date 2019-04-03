e905ff376d80304ef516be6563159335b9fd9157
李明 <liming@terminus.io>
2019-04-03 17:45:32 +0800

    ss

---
+++ src/main/java/com/uhomed/jenkins/docker/Application.java
@@ -0,0 +1,9 @@
+package com.uhomed.jenkins.docker;
+
+/**
+ * @author liming
+ * @version 1.0
+ * @date Apr 2019-04-03 17:40
+ */
+public class Application {
+}
---
+++ src/main/java/com/uhomed/jenkins/docker/TestController.java
@@ -0,0 +1,9 @@
+package com.uhomed.jenkins.docker;
+
+/**
+ * @author liming
+ * @version 1.0
+ * @date Apr 2019-04-03 17:42
+ */
+public class TestController {
+}
